3b060b00aa8bd2e151b2a4b3dd4d43d945f34d4f
leesoli0122 <leesoli0122@gmail.com>
2025-08-13 11:43:53 +0900

    5단계 남았음

--- javascript/js/day_1_dom_p.js
+++ javascript/js/day_1_dom_p.js
@@ -48,3 +48,94 @@ function resetColor() {
     box.style.border = '2px solid #e2e8f0';
     showSuccess('step2', '색상을 초기화했습니다!');
 }
+
+// 3단계
+// 그대로 표시
+function displayInput() {
+    const input = document.getElementById('userInput');
+    const display = document.getElementById('displayBox');
+
+    if (input.value.trim() === '') { //만약 인풋에 아무 값이 없다면 알럿을 띄워주세요
+        alert('메시지를 입력해주세요');
+        return;
+    }
+
+    display.textContent = input.value;
+    showSuccess('step3', '입력값을 그대로 표시했습니다');
+}
+// 인사와 함께
+function displayWithGreeting() {
+    const input = document.getElementById('userInput');
+    const display = document.getElementById('displayBox');
+
+    if (input.value.trim() === '') { //만약 인풋에 아무 값이 없다면 알럿을 띄워주세요
+        alert('메시지를 입력해주세요');
+        return;
+    }
+
+    display.textContent = `안녕하세요 "${input.value}"라고 하셨군요`;
+    showSuccess('step3', '인사말과 함께 표시했습니다');
+}
+// 대문자로
+function displayUppercase() {
+    const input = document.getElementById('userInput');
+    const display = document.getElementById('displayBox');
+
+    if (input.value.trim() === '') {
+        alert('메시지를 입력해주세요');
+        return;
+    }
+
+    display.textContent = input.value.toUpperCase();
+    showSuccess('step3', '대문자로 표시했습니다');
+}
+// 글자 수 세기
+function displayLength() {
+    const input = document.getElementById('userInput');
+    const display = document.getElementById('displayBox');
+
+    if (input.value.trim() === '') {
+        alert('메시지를 입력해주세요');
+        return;
+    }
+
+    display.textContent = `"${input.value}"는 총 "${input.value.length}"글자 입니다`;
+    showSuccess('step3', '글자 수를 계산해서 표시했습니다');
+}
+// 입력창 지우기
+function clearInput() {
+    document.getElementById('userInput').value = '';
+    document.getElementById('displayBox').textContent = '입력한 내용이 여기에 표시됩니다';
+    showSuccess('step3', '입력창을 지웠습니다!');
+}
+
+// 4단계
+function increment(amount = 1) {
+    counter += amount;
+    document.getElementById('counter').textContent = counter;
+    showSuccess('step4', `카운터를 +${counter} 증가시켰습니다.(현재 ${counter})`);
+}
+function decrement(amount = 1) {
+    counter -= amount;
+    document.getElementById('counter').textContent = counter;
+    showSuccess('step4', `카운터를 -${counter} 감소시켰습니다.(현재 ${counter})`);
+}
+function resetCounter() {
+    counter = 0;
+    document.getElementById('counter').textContent = counter;
+    showSuccess('step4', '카운터를 0으로 초기화했습니다');
+}
+
+// 5단계
+function addItem() {
+    const input = document.getElementById('itemInput');
+    const container = document.getElementById('itemContainer');
+
+    let itemName
+    if (input.value.trim() === '') {
+        itemName = `아이템 ${++itemCount}`;
+    } else {
+        itemName = input.value.trim();
+        itemCount++;
+    }
+}
