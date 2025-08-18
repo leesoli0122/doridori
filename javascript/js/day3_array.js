2852147e3bae64872a383e30cf8725030e536682
leesoli0122 <leesoli0122@gmail.com>
2025-08-18 16:08:03 +0900

    html 이름 변경

---
+++ javascript/js/day3_array.js
@@ -0,0 +1,9 @@
+// step1 기본 배열 조작
+function step1() {
+    const output = document.getElementById('output1');
+    let result = '';
+
+    // 메뉴 이름들을 배열로 관리
+    const menuNames = ['홈', '서비스', '포트폴리오'];
+    result += `초기 메뉴: ${JSON.stringify(menuNames)}\n`;
+}
